e2e66d474506ce620de04421bedaa59da38cfbb9
pierrelmty <pierre.lemetayer@etu.univ-nantes.fr>
2015-03-05 11:49:17 +0100

    Datamain and main in progress

--- main.py
+++ main.py
@@ -24,10 +24,38 @@ for row in resultE:
 	print(str(row.number) + " ; " + str(row.name))
 """
 
-	
+"""	
 rdI = ReadJSON("data/installations.json")
 rdI.readInstallation()
 resultI = rdI.getResult()
 
 for row in resultI:
 	print(str(row.number) + " ; " + str(row.name) + " ; " + str(row.address) + " ; " + str(row.zipCode) + " ; " + str(row.city) + " ; " + str(row.latitude) + " ; " + str(row.longitude))
+"""
+
+print("Creation of activity database...")
+database = Database("data/activities.json")
+database.createDB()
+print("Database created")
+
+print("Creation of equipment database...")
+database = Database("data/equipments.json")
+database.createDB()
+print("Database created")
+
+print("Creation of installation database...")
+database = Database("data/installations.json")
+database.createDB()
+print("Database created")
+
+print("insertion in activity database...")
+database.InsertInActivity()
+print("Insertion done")
+
+print("insertion in equipment database...")
+database.InsertInEquipment()
+print("Insertion done")
+
+print("insertion in installation database...")
+database.InsertInInstallation()
+print("Insertion done")
--- services/database.py
+++ services/database.py
@@ -4,11 +4,9 @@ import sqlite3
 
 class Database:
 
-	def __init__(self, path, name):
+	def __init__(self, path):
 		self.conn = sqlite3.connect(path)
 		self.path = path
-		self.name = name
-
 		
 		
 	def createDB(self):
@@ -25,18 +23,25 @@ class Database:
 		c.execute("CREATE TABLE installation (numero INTEGER, nom VARCHAR, adresse VARCHAR, code_postal INTEGER, ville VARCHAR, latitude DECIMAL, longitude DECIMAL)")
 
 		self.conn.commit()
+
 		
 	def InsertInActivity(self, activite):
-		c = self.conn.cursor()
-		c.execute('INSERT INTO activite VALUES()')
+                c = self.conn.cursor()
+		c.execute('''INSERT INTO activite(name, number) VALUES(:name, :number)''',
+                          {'name':activite.name, 'number':activite.number})
+
 		
-	def InsertInActivity(self, equipement):
+	def InsertInEquipment(self, equipement):
 		c = self.conn.cursor()
-		c.execute('INSERT INTO equipement VALUES()')
+		c.execute('''INSERT INTO equipement(number, name, installationNumber) VALUES(:number, :name, :installationNumber) VALUES(:name, :number, :installationNumber)''',
+                          {'number':equipement.number, 'name':equipement.name, 'installationNumber':equipement.installationNumber})
+
 		
-	def InsertInActivity(self, installation):
+	def InsertInInstallation(self, installation):
 		c = self.conn.cursor()
-		c.execute('INSERT INTO installation VALUES()')
+		c.execute('''INSERT INTO installation(number, name, address, zipCode, city, latitude, longitude) VALUES(:number, :name: address, :zipCode, :city, :latitude, :longitude)''',
+                          {'number':installation.number, 'name':installation.name, 'address':installation.address, 'zipCode':installation.zipCode, 'city':installation.city, 'latitude':installation.latitude, 'longitude':installation.longitude})
+
 	
 	def disconnect(self):
 	
